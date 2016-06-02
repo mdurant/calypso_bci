4e09de64554a2ad28c6d3cf3b47918713ab56e2c
Mauricio Durán Torres <mauriciodurant@gmail.com>
2016-06-02 21:57:26 +0000

    se agrega image al model de team_profile

--- app/models/profile_team.rb
+++ app/models/profile_team.rb
@@ -1,2 +1,9 @@
 class ProfileTeam < ActiveRecord::Base
+    
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :anexo, presence: true
+    
+    
+    mount_uploader :image, ImageUploader
 end
