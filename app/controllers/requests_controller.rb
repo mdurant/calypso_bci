d8e7a2ebd275ecdfaeb2d2c828d7b68c05b0fd60
Mauricio Durán Torres <mauriciodurant@gmail.com>
2016-08-05 20:16:38 +0000

    mejora en controller blog y categories, adicionalmente welcome#home para visualizar noticias

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,6 @@
 class RequestsController < ApplicationController
   before_action :set_request, only: [:show, :edit, :update, :destroy]
+  
 
   # GET /requests
   # GET /requests.json
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,5 +3,7 @@ class Blog < ActiveRecord::Base
   validates_presence_of :title, :fecha, :noticia, :category_id
   
   belongs_to :category, foreign_key: "category_id"
-  belongs_to :user, foreign_key: "id_user"
+  belongs_to :user, foreign_key: "user_id"
+  
+  mount_uploader :foto, ImageUploader
 end
