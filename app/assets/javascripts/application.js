6b023a9646e73fa103e0612eac27398449abef48
Mauricio Durán Torres <mauriciodurant@gmail.com>
2016-08-16 00:22:21 +0000

    ckeditor in applicaction.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,5 @@
 //= require bootstrap-datepicker/core
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.es.js
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.fr.js
-
 //= require ckeditor/init
 //= require_tree .
